fe77480845395d3f47b7b9d4ba12f3ae8252386a
Torres Remy <remy.torres@etu.u-bordeaux.fr>
2018-01-30 11:49:49 +0100

    rename nom + prénom

--- db/migrate/21180109083218_create_users.rb
+++ db/migrate/21180109083218_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-      add_column :users, :name, :string
-      add_column :users, :surname, :string
+      add_column :users, :first_name, :string
+      add_column :users, :last_name, :string
   end
 end
