61c76b28bd1d357fbd269b89582ac4def3c2ba3b
Sevin Evan <evan.sevin@etu.u-bordeaux.fr>
2018-01-30 11:48:35 +0100

    fix merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,4 @@
 class User < ActiveRecord::Base
-<<<<<<< HEAD
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-=======
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable,
@@ -16,5 +9,4 @@ class User < ActiveRecord::Base
     def fullname
         surname+" "+name
     end
->>>>>>> 2432d49a95c5631328a220421e71b1f1ee4127df
 end
